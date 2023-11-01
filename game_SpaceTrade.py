2a863789ec369e32ab34168e73804ab58c302fce
Daniel Alm D'Adda <swedmarine@hotmail.com>
2023-11-01 14:53:37 +0100

    Partial rewrite

--- game_SpaceTrade.py
+++ game_SpaceTrade.py
@@ -8,62 +8,219 @@
 
 import random
 
-dict_goods = {'foodstuffs':2,
-              'materials':2,
-              'technologies':3,
-              'entertainments':4,
-              'luxuries':6}
 
-dict_planets = {'space station':{},
-                'techon'  :{'technologies':-2,
-                            'entertainments':-2,
-                            'foodstuffs':2},
-                'glamille':{'entertainments':-2,
-                            'luxuries':-2,
-                            'materials':2},
-                'automic' :{'luxuries':-2,
-                            'foodstuffs':-2,
-                            'technologies':2},
-                'grunto'  :{'foodstuffs':-2,
-                            'materials':-2,
-                            'entertainments':2},
-                'laboria' :{'materials':-2,
-                            'technologies':-2,
-                            'luxuries':2},
-                'robosol' :{'foodstuffs':-2,
-                            'technologies':-2,
-                            'materials':2},
-                'bultor'  :{'materials':-2,
-                            'entertainments':-2,
-                            'technologies':2},
-                'misera'  :{'technologies':-2,
-                            'luxuries':-2,
-                            'entertainments':2},
-                'beverill':{'entertainments':-2,
-                            'foodstuffs':-2,
-                            'luxuries':2},
-                'monor'   :{'luxuries':-2,
-                            'materials':-2,
-                            'foodstuffs':2}}
+class Good:
+    def __init__(self, name: str, base_price: int) -> None:
+        self._name = name
+        self._base_price = base_price
 
-dict_players = {}
+    @property
+    def name(self) -> str:
+        return self._name
+    
+    @property
+    def base_price(self) -> int:
+        return self._base_price
 
-unused_planets = []
-for planet in dict_planets.keys():
-    if planet != 'space station':
-        unused_planets.append(planet)
-planet_system = ['space station']
-while len(planet_system) < 7:
-    planet_system.append(unused_planets.pop(random.randint(0,len(unused_planets)-1)))
 
-supply_goods = []
-for good in dict_goods.keys():
-    count = 0
-    while count < 5:
-        supply_goods.append(good)
-        count += 1
+class Supply:
+    def __init__(self) -> None:
+        self._all_goods: list[Good] = []
+        for _ in range(0, 5):
+            self._all_goods.append(Good('foodstuffs', 2))
+            self._all_goods.append(Good('materials', 2))
+            self._all_goods.append(Good('technologies', 3))
+            self._all_goods.append(Good('entertainments', 4))
+            self._all_goods.append(Good('luxuries', 6))
 
-def bot_turn(player, victory_condition):
+    def fetch_market(self) -> list[Good]:
+        market: list[Good] = []
+        random.shuffle(self._all_goods)
+        while len(market) < 5:
+            market.append(self._all_goods.pop(0))
+        return market
+
+    def return_market(self, market: list[Good]) -> None:
+        self._all_goods.extend(market)
+
+    # def grab_random_good(self):
+    #    random.shuffle(self._all_goods)
+    #    return self._all_goods[0]
+
+
+class World:
+    def __init__(self, name: str,
+                       exports: list[str],
+                       imports: list[str]) -> None:
+        self._name = name
+        self._exports = exports
+        self._imports = imports
+
+    @property
+    def name(self) -> str:
+        return self._name
+    
+    @property
+    def exports(self) -> list[str]:
+        return self._exports
+    
+    @property
+    def imports(self) -> list[str]:
+        return self._imports
+
+
+class Player:
+    def __init__(self, name: str, human: bool) -> None:
+        self._name = name
+        self._is_human = human
+        self._location_index: int = 0
+        self._credits: int = 5
+        self._cargo: list[Good] = []
+    
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @property
+    def is_human(self) -> bool:
+        return self._is_human
+    
+    @property
+    def location_index(self) -> int:
+        return self._location_index
+    
+    @property
+    def credits(self) -> int:
+        return self._credits
+    
+
+class Game:
+    _world_names = ['Ezyria', 'Oclite', 'Stradus VI', 'Noxonov'
+                    'Amustea', 'Trikanov', 'Veavis', 'Kalvarth']
+
+    def __init__(self, player_amount: int, bot_amount: int) -> None:
+        self._players: list[Player] = []
+        self._worlds = [World('Station VBY-219', [], [])]
+        self._supply = Supply()
+        self._victory_condition: int = 15
+        
+        while len(self._players) < player_amount:
+            player_num = len(self._players) + 1
+            name = input(f'Input name of player #{player_num}: ')
+            self._players.append(Player(name, True))
+        for bot_num in range(0, bot_amount):
+            self._players.append(Player(f'Bot #{bot_num}', False))
+        
+        while len(self._worlds) < 7:
+            random.shuffle(self._world_names)
+            name = self._world_names.pop(0)
+            good_names = list(random.shuffle('foodstuffs',
+                                             'materials',
+                                             'technologies',
+                                             'entertaiments',
+                                             'luxuries'))
+            exports = [good_names.pop(0), good_names.pop(0)]
+            imports = [good_names.pop(0)]
+            self._worlds.append(World(name, exports, imports))
+
+        self._active_player = self._players[-1]
+    
+    def main(self) -> None:
+        while self._no_victory():
+            self._next_player()
+            print(f'====== NEW TURN - Player {self._active_player.name} ======')
+            if self._active_player:
+                self._player_turn()
+            else:
+                bot_turn()
+        print(f'Congratulations! Player {self._active_player.name} has won!')
+
+    def _next_player(self) -> None:
+        next_player_index = self._players.index(self._active_player) + 1
+        if next_player_index == len(self._players):
+            next_player_index = 0
+        self._active_player = self._players[next_player_index]
+    
+    def _player_turn(self) -> None:
+        location = self._player_movement()
+        print(f'You arrive at the market in {self._worlds[self._active_player.location_index]}.')
+        self._player_buy(location)
+
+    def _player_movement(self) -> None:
+        reach = random.randint(1, 3)
+        for world in self._worlds:
+            print(world, end='')
+            if self._active_player.location_index == self._worlds.index(world):
+                print(' <-- Current Location')
+            else:
+                print()
+        print(f'You are able to move {reach} steps this turn.')
+        while True:
+            destination = input('Input name of destination: ').lower()
+            for world in self._worlds:
+                if (world.name.lower() == destination and not
+                    abs(self._worlds.index(world) - self._active_player.location_index) > reach):
+                    self._active_player.location_index = self._worlds.index(world)
+                    destination = 'DONE'
+            if destination == 'DONE':
+                break
+            else:
+                print('Invalid destination.')
+        return self._worlds[self._active_player.location_index]
+    
+    def _player_buy(self, location: World) -> None:
+        market_inventory = self._supply.fetch_market()
+        market: dict[str, list[int, str]] = {}
+        for good in market_inventory:
+            if good.name in location.exports:
+                market[good.name] = [good.base_price - 2, ' Cheap!']
+            elif good.name in location.imports:
+                market[good.name] = [good.base_price + 2, ' Expensive!']
+            else:
+                market[good.name] = [good.base_price, '']
+        
+        for good in market_inventory:
+
+
+                    
+
+    def _no_victory(self):
+        if self._active_player.credits < self._victory_condition:
+            return True
+        else:
+            return False
+
+def run():
+    print('()=====(SPACE TRADERS)=====()')
+    print('1. Start Game')
+    print('2. Quit Game')
+    while True:
+        opt = input('>>> ')
+        if opt == '1':
+            break
+        elif opt == '2':
+            quit()
+        else:
+            print('Invalid option.')
+    while True:
+        player_amount = input('How many people are playing? ')
+        if player_amount.isdecimal() and int(player_amount) > 0:
+            break
+        else:
+            print('Invalid amount.')
+    while True:
+        bot_amount = input('How many bots would you like to add? ')
+        if bot_amount.isdecimal():
+            break
+        else:
+            print('Invalid amount.')
+    game = Game(player_amount, bot_amount)
+    game.main()
+
+if __name__ == '__main__':
+    run()
+# ============================================================================
+def _bot_turn(player, victory_condition):
     location = dict_players[player][0]
     credits = dict_players[player][1]
     cargo = dict_players[player][2]
@@ -166,47 +323,7 @@ def establish_market(location):
         market_prices[good] += dict_planets[planet_system[location]][good]
     return market_prices    
 
-def main():
-    start_game()
-    active_player = 0
-    victory = False
-    victory_condition = 20
-    while not victory:
-        if active_player == 'bot':
-            active_player = 1
-        else:
-            active_player += 1
-        if active_player not in dict_players.keys():
-            if 'bot' in dict_players.keys():
-                active_player = 'bot'
-            else:
-                active_player = 1
-        if active_player == 'bot':
-            victory = bot_turn(active_player, victory_condition)
-        else:
-            victory = player_turn(active_player, victory_condition)
-    print(f'Congratulations player {active_player}! You won!')
-
-def movement(origin):
-    moved = False
-    move_dice = random.randint(1,3)
-    for planet in planet_system:
-        print(planet.capitalize(),end=' ')
-        if origin == planet_system.index(planet):
-            print('<--YOU ARE HERE')
-        else:
-            print()
-    while not moved:
-        destination = input(f'You are able to move {move_dice} steps this turn. Where would you like to go? ').lower()
-        if destination in planet_system:
-            if abs(planet_system.index(destination) - origin) > move_dice:
-                print('That destination is beyond your reach. Try again.')
-            else:
-                return planet_system.index(destination)
-        else:
-            print('That destination does not exist. Try again.')
-
-def player_turn(player, victory_condition):
+def _player_turn(player, victory_condition):
     location = dict_players[player][0]
     credits = dict_players[player][1]
     cargo = dict_players[player][2]
@@ -244,24 +361,3 @@ def selling(location, credits, cargo):
             else:
                 input('Invalid option. Press ENTER to try again.')
     return credits, cargo
-
-def start_game():
-    while True:
-        player_count = input('How many are playing? ')
-        if player_count.isnumeric():
-            if int(player_count) > 0:
-                player_count = int(player_count)
-                bot_playing = input('Do you wish to add a bot?(y/n) ').lower()
-                if bot_playing == 'y':
-                    dict_players['bot'] = [0,5,[]]
-                break
-            else:
-                print('No players. Closing game.')
-                quit()
-        else:
-            print('Invalid input. Needs to be an integer.')
-    for player in range(player_count):
-        dict_players[player+1] = [0,5,[]]
-
-if __name__ == '__main__':
-    main()
