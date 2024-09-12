e7cb12a1d35275b15e73f99b84f2dc88bdec8bf5
Daniel Alm D'Adda <swedmarine@hotmail.com>
2024-09-12 09:31:59 +0200

    morning lecture

---
+++ EffectivePython/lesson8_challenges/two_sum.py
@@ -0,0 +1,48 @@
+from timeit import default_timer
+
+def two_sum(nums: list[int], target: int) -> list[int]:
+    for index1, num1 in enumerate(nums):
+        for index2, num2 in enumerate(nums):
+            if index1 == index2:
+                break
+            elif num1 + num2 == target:
+                return [index1, index2]
+            
+def two_sum_2(nums: list[int], target: int) -> list[int]:
+    sorted_nums = sorted(nums)
+    index1 = 0
+    index2 = len(sorted_nums)
+    while index1 < index2:
+        sum_ = sorted_nums[index1] + sorted_nums[index2]
+        if sum_ == target:
+            return [index1, index2]
+        if sum_ > target:
+            index2 -= 1
+        elif sum_ < target:
+            index1 += 1
+
+tests = [
+    dict(
+        nums = [2,7,11,15],
+        target = 9,
+        output = [0,1]
+    ),
+    dict(
+        nums = [3,2,4],
+        target = 6,
+        output = [1,2]
+    ),
+    dict(
+        nums = [3,3],
+        target = 6,
+        output = [0,1]
+    )
+]
+
+for func in [two_sum, two_sum_2]:
+    print(func.__name__)
+    start = default_timer()
+    for test in tests:
+        result = two_sum(nums = test['nums'], target = test['target'])
+        print(set(result) == set(test['output']))
+    print('Time taken:', default_timer() - start)
