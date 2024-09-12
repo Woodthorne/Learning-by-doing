74c37d40bc259a20466796ef31a394af8d03f62a
Daniel Alm D'Adda <swedmarine@hotmail.com>
2024-09-12 11:08:52 +0200

    Solution added

--- EffectivePython/lesson8_challenges/longest_prefix.py
+++ EffectivePython/lesson8_challenges/longest_prefix.py
@@ -13,6 +13,20 @@ def longest_common_prefix(strs: list[str]) -> str:
     else:
         return ''
 
+# Given by instructor
+def longest_common_prefix_solved(strs: list[str]) -> str:
+    sorted_strs = sorted(strs)
+    first = sorted_strs[0]
+    last = sorted_strs[-1]
+    
+    prefix = ''
+    for index in range(min(len(first), len(last))):
+        if first[index] == last[index]:
+            prefix += first[index]
+        else:
+            break
+    return prefix
+
 tests = [
     dict(
         strs = ['flower', 'flow', 'flight'],
@@ -24,7 +38,7 @@ tests = [
     )
 ]
 
-for func in [longest_common_prefix]:
+for func in [longest_common_prefix, longest_common_prefix_solved]:
     print(func.__name__)
     for test in tests:
         result = func(test['strs'])
