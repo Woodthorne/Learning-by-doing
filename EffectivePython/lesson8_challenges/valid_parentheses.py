650b974a185fc7b6c97d1fcb240a06dcdaeb4e02
Daniel Alm D'Adda <swedmarine@hotmail.com>
2024-09-12 11:17:55 +0200

    first solution

---
+++ EffectivePython/lesson8_challenges/valid_parentheses.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isValid(sefl, s: str) -> bool:
+        active_brackets = []
+        closing = {
+            ')': '(',
+            '}': '{',
+            ']': '['
+        }
+        for char in s:
+            if char in ['(', '{', '[']:
+                active_brackets.append(char)
+            else:
+                if closing[char] == active_brackets[-1]:
+                    active_brackets.pop()
+                else:
+                    return False
+        return not active_brackets
