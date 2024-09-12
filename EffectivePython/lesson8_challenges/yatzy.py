f257277015f09d2f16f3ba80fe420c75f51ed6ec
Daniel Alm D'Adda <swedmarine@hotmail.com>
2024-09-12 12:33:50 +0200

    Added slots up to Pairs

--- EffectivePython/lesson8_challenges/yatzy.py
+++ EffectivePython/lesson8_challenges/yatzy.py
@@ -3,7 +3,7 @@ from random import randint as roll_die
 class Slot:
     def __init__(self) -> None:
         self._value = None
-        self.blocked = False
+        self._blocked = False
 
     @property
     def value(self) -> int:
@@ -12,16 +12,25 @@ class Slot:
         else:
             return self._value
     
-    def score(self, dice: list[int]) -> bool:
-        # Logic for verifying scoring
-        pass
+    @property
+    def blocked(self) -> bool:
+        return self._blocked
 
-class Ones(Slot):
-    def __init__(self) -> None:
-        super().__init__()
+    def block(self) -> bool:
+        if self.blocked:
+            return False
+        else:
+            self._blocked = True
+            return True
     
     def score(self, dice: list[int]) -> bool:
-        if self.value == 0:
+        # Logic for verifying scoring
+        return False
+
+
+class Ones(Slot):    
+    def score(self, dice: list[int]) -> bool:
+        if self.value == 0 and not self.blocked:
             total = sum(die for die in dice if die == 1)
             if total > 0:
                 self._value = total
@@ -29,6 +38,99 @@ class Ones(Slot):
         return False
 
 
+class Twos(Slot):
+    def score(self, dice: list[int]) -> bool:
+        if self.value == 0 and not self.blocked:
+            total = sum(die for die in dice if die == 2)
+            if total > 0:
+                self._value = total
+                return True
+        return False
+
+
+class Threes(Slot):
+    def score(self, dice: list[int]) -> bool:
+        if self.value == 0 and not self.blocked:
+            total = sum(die for die in dice if die == 3)
+            if total > 0:
+                self._value = total
+                return True
+        return False
+
+
+class Fours(Slot):
+    def score(self, dice: list[int]) -> bool:
+        if self.value == 0 and not self.blocked:
+            total = sum(die for die in dice if die == 4)
+            if total > 0:
+                self._value = total
+                return True
+        return False
+
+
+class Fives(Slot):
+    def score(self, dice: list[int]) -> bool:
+        if self.value == 0 and not self.blocked:
+            total = sum(die for die in dice if die == 5)
+            if total > 0:
+                self._value = total
+                return True
+        return False
+
+
+class Sixes(Slot):
+    def score(self, dice: list[int]) -> bool:
+        if self.value == 0 and not self.blocked:
+            total = sum(die for die in dice if die == 6)
+            if total > 0:
+                self._value = total
+                return True
+        return False
+
+
+class Bonus(Slot):
+    def verify(self, upper_scores: list[int]) -> bool:
+        if self.value == 0 and not self.blocked:
+            total_upper = sum(upper_scores)
+            if total_upper >= 63:
+                self._value = 50
+                return True
+        return False
+
+
+class Pair(Slot):
+    def score(self, dice: list[int]) -> bool:
+        if self.value == 0 and not self.blocked:
+            sorted_dice = sorted(dice, reverse = True)
+            for index in range(1, len(sorted_dice)):
+                die_1 = sorted_dice[index]
+                die_2 = sorted_dice[index - 1]
+                if die_1 == die_2:
+                    self._value = die_1 + die_2
+                    return True
+        return False
+
+
+class Pairs(Slot):
+    def score(self, dice: list[int]) -> bool:
+        if self.value == 0 and not self.blocked:
+            sorted_dice = sorted(dice, reverse = True)
+            scored = None
+            score_1 = None
+            score_2 = None
+            for index in range(1, len(sorted_dice)):
+                die_1 = sorted_dice[index]
+                die_2 = sorted_dice[index - 1]
+                if die_1 == die_2:
+                    if not scored:
+                        score_1 = die_1 + die_2
+                        scored = [index, index - 1]
+                    elif index not in scored and index - 1 not in scored:
+                        score_2 = die_1 + die_2
+            if score_1 and score_2:
+                self._value = score_1 + score_2
+                return True
+        return False
 
 
 
@@ -76,18 +178,4 @@ class Ones(Slot):
         
 #         return total_score
     
-#     def verify_bonus(self) -> None:
-#         requirements = [
-#             'ones', 'twos', 'threes',
-#             'fours', 'fives', 'sixes'
-#         ]
-#         bonus_possible = True
-#         for slot in requirements:
-#             if not self.scores[slot]:
-#                 bonus_possible = False
-#         if bonus_possible:
-#             total_upper = sum(self.scores[slot] for slot in requirements)
-#             if total_upper < 63:
-#                 self.scores['bonus'] = 'blocked'
-#             else:
-#                 self.scores['bonus'] = 50
+#     
