595ccc719f5d70c064f8be686137a45832fdd8f1
Daniel Alm D'Adda <swedmarine@hotmail.com>
2024-09-18 11:16:02 +0200

    Moved everything but imports into if __name__ statement.

--- EffectivePython/lesson8_challenges/yatzy/yatzy.py
+++ EffectivePython/lesson8_challenges/yatzy/yatzy.py
@@ -1,8 +1,8 @@
 from game_logic import YatzyGame
 from interface import Interface
 
-game = YatzyGame()
-interface = Interface(game = game)
 
 if __name__ == '__main__':
+    game = YatzyGame()
+    interface = Interface(game = game)
     interface.main_menu()
