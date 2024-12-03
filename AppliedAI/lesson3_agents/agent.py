d5e3297670da52c7132c321ee06bda50cfeac4e8
Daniel Alm D'Adda <swedmarine@hotmail.com>
2024-12-03 11:32:14 +0100

    Added class Environment

--- AppliedAI/lesson3_agents/agent.py
+++ AppliedAI/lesson3_agents/agent.py
@@ -25,36 +25,47 @@ class VacuumAgent:
             case _:
                 raise ValueError('Unrecognised percept values.')
 
+
+class Environment:
+    def __init__(self, dirty_chance: int = 50) -> None:
+        self.grid = [
+            {'name': 'A', 'state': 'Clean'},
+            {'name': 'B', 'state': 'Clean'}
+        ]
+        self.random = iter(quasirandom_percentage(dirty_chance))
+    
+    def dirtify(self) -> None:
+        for space in self.grid:
+            if next(self.random):
+                space['state'] = 'Dirty'
+
+
+env = Environment()
 bot = VacuumAgent()
-grid = [
-    {'name': 'A', 'state': 'Clean'},
-    {'name': 'B', 'state': 'Clean'}
-]
-location = 0
+agent_location = 0
 score = 0
 random = iter(quasirandom_percentage(50))
 for _ in range(1_000):
-    for space in grid:
-        if next(random):
-            space['state'] = 'Dirty'
-    
-    percept = tuple(grid[location].values())
+    env.dirtify()
+
+    percept = tuple(env.grid[agent_location].values())
     match bot.action(percept):
         case ActionEnum.Suck:
             print('Cleaning')
-            grid[location]['state'] = 'Clean'
+            env.grid[agent_location]['state'] = 'Clean'
         case ActionEnum.Right:
-            if location != 1:
+            if agent_location != 1:
                 print('Moving')
-                location = 1
+                agent_location = 1
         case ActionEnum.Left:
-            if location != 0:
+            if agent_location != 0:
                 print('Moving')
-                location = 0
+                agent_location = 0
         case _:
             print('Idling')
 
-    for space in grid:
-        score += len([space for space in grid if space['state'] == 'Clean'])
+    for space in env.grid:
+        score += len([space for space in env.grid
+                      if space['state'] == 'Clean'])
 
 print(f'{score=}')
