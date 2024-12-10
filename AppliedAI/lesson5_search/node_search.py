cde1ead29a1ade9dfb73db76d26081740e828fba
Daniel Alm D'Adda <swedmarine@hotmail.com>
2024-12-10 14:19:47 +0100

    renamed path to edge

--- AppliedAI/lesson5_search/node_search.py
+++ AppliedAI/lesson5_search/node_search.py
@@ -5,13 +5,13 @@ from string import ascii_lowercase
 class Node:
     def __init__(self, name: str):
         self.name = name
-        self.paths: list[Path] = []
+        self.edges: list[Edge] = []
     
     def __repr__(self):
         return f'Node(name={self.name})'
 
 
-class Path:
+class Edge:
     def __init__(self, nodes: set[Node], length: int):
         self.nodes = nodes
         self.length = length
@@ -36,26 +36,26 @@ def random_network(n_nodes: int, n_paths: int) -> None:
     unlinked_nodes = nodes.copy()
     random.shuffle(unlinked_nodes)
     node = unlinked_nodes.pop()
-    paths = 0
+    edges = 0
     while unlinked_nodes:
         companion = unlinked_nodes.pop()
-        path = Path({node, companion})
-        paths += 1
-        node.paths.append(path)
-        companion.paths.append(path)
+        edge = Edge({node, companion})
+        edges += 1
+        node.edges.append(edge)
+        companion.edges.append(edge)
         node = companion
     
-    while paths < n_paths:
+    while edges < n_paths:
         node = random.choice(nodes)
         companion = random.choice(nodes)
         if node == companion:
             continue
-        if {node, companion} in [path.nodes for path in node.paths]:
+        if {node, companion} in [edge.nodes for edge in node.edges]:
             continue
-        path = Path({node, companion})
-        paths += 1
-        node.paths.append(path)
-        companion.paths.append(path)
+        edge = Edge({node, companion})
+        edges += 1
+        node.edges.append(edge)
+        companion.edges.append(edge)
     
     return nodes
 
@@ -76,13 +76,13 @@ def ucs(start: Node, end: Node) -> tuple[int, list[Node]]|None:
 
         expanded.add(node)
 
-        for path in node.paths:
-            [neighbour] = path.nodes.difference({node})
+        for edge in node.edges:
+            [neighbour] = edge.nodes.difference({node})
             
             if neighbour in expanded:
                 continue
 
-            new_length = node_info[0] + path.length
+            new_length = node_info[0] + edge.length
             new_route = node_info[1].copy()
             new_route.append(neighbour)
             queue.append((new_length, new_route))
@@ -100,25 +100,25 @@ if __name__ == '__main__':
         'G': Node('G'),
     }
 
-    paths = {
-        'S1': Path({nodes['S'], nodes['1']}, 2),
-        'S3': Path({nodes['S'], nodes['3']}, 5),
-        '13': Path({nodes['1'], nodes['3']}, 5),
-        '1G': Path({nodes['1'], nodes['G']}, 1),
-        '12': Path({nodes['1'], nodes['2']}, 4),
-        '24': Path({nodes['2'], nodes['4']}, 4),
-        '25': Path({nodes['2'], nodes['5']}, 6),
-        '3G': Path({nodes['3'], nodes['G']}, 6),
-        '34': Path({nodes['3'], nodes['4']}, 2),
-        '45': Path({nodes['4'], nodes['5']}, 3),
-        '4G': Path({nodes['4'], nodes['G']}, 7),
-        '5G': Path({nodes['5'], nodes['G']}, 3),
+    edges = {
+        'S1': Edge({nodes['S'], nodes['1']}, 2),
+        'S3': Edge({nodes['S'], nodes['3']}, 5),
+        '13': Edge({nodes['1'], nodes['3']}, 5),
+        '1G': Edge({nodes['1'], nodes['G']}, 1),
+        '12': Edge({nodes['1'], nodes['2']}, 4),
+        '24': Edge({nodes['2'], nodes['4']}, 4),
+        '25': Edge({nodes['2'], nodes['5']}, 6),
+        '3G': Edge({nodes['3'], nodes['G']}, 6),
+        '34': Edge({nodes['3'], nodes['4']}, 2),
+        '45': Edge({nodes['4'], nodes['5']}, 3),
+        '4G': Edge({nodes['4'], nodes['G']}, 7),
+        '5G': Edge({nodes['5'], nodes['G']}, 3),
     }
 
-    for node_names, path in paths.items():
+    for node_names, edge in edges.items():
         node_1, node_2 = list(node_names)
-        nodes[node_1].paths.append(path)
-        nodes[node_2].paths.append(path)
+        nodes[node_1].edges.append(edge)
+        nodes[node_2].edges.append(edge)
     
     start = 'S'
     end = 'G'
